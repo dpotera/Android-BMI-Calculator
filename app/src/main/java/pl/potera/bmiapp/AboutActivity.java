b151747d00dd8b8df826936d1b8251a34fabe93b
dpotera <dominik.potera@gmail.com>
2017-04-05 00:34:36 +0200

    Horizontal layout

---
+++ app/src/main/java/pl/potera/bmiapp/AboutActivity.java
@@ -0,0 +1,15 @@
+package pl.potera.bmiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.potera.bmiapp.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
